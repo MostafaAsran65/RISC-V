8b8adb26813655be5696838ab1d77bb2a58d2ef6
MostafaAsran65 <mostafaasran656@gmail.com>
2025-10-30 18:18:23 +0300

    creat a new Node Nodjson

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 function greet (name) {
-    return 'Hello ,$(name)! ';
+    const newLocal = 'Hello, $(name)! ';
+    return newLocal;
 }
 module.exports = greet;// Ensure function is exportable
 
 // If run directly,print output (useful for debugging)
 
-if (require. main== module){
+if (require. main== module) {
 
   console.log (greet ( "World") ) ;  
 }
-
