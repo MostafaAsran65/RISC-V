9744f6c7f27b3124e15728ecbe5102d6a2e14432
MostafaAsran65 <mostafaasran656@gmail.com>
2025-10-30 18:09:45 +0300

    creat a new Node Nodjson

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#src/test.sh
+
+
+
+EXPECTED="Hello, Test!"
+OUTPUT= $(node -e "console.log(require('./src/app')('Test')")
+if [ "$OUTPUT" == "$EXPECTED" ]; then
+  echo "O Test passed"
+  exit
+else
+  echo "💯 Test failed! Expected"
+
+exit 0
+else
+echo "X Test failed! Expected '$EXPECTED'but got '$OUTPUT'"
+exit 1
+fi
