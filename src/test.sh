b531fb5cd114fc7335f96547e21953eb4c30d63b
MostafaAsran65 <mostafaasran656@gmail.com>
2025-10-30 19:51:27 +0300

    Fixed all workflow paths and files

--- src/test.sh
+++ src/test.sh
@@ -5,7 +5,7 @@
 
 EXPECTED= "Hello,Test!"
 OUTPUT= $(node -e "console.log(require('./app')('Test'))")
-if [ "$OUTPUT" == "$EXPECTED" ]; then
+if [ "$OUTPUT"=="$EXPECTED" ]; then
 
   echo "Test passed"
 
